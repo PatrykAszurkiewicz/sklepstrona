f5a26a38581dbbd77a869a30c77308f13f5f9b32
PatrykAszurkiewicz <patrykos65a@gmail.com>
2023-01-25 00:25:54 +0100

    fix5

--- backend/connection.js
+++ backend/connection.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionStr = "mongodb+srv://admin:grzyb123@cluster0.2pxasg0.mongodb.net/?retryWrites=true&w=majority";
+const connectionStr =
+  "mongodb+srv://admin:grzyb123@cluster0.2pxasg0.mongodb.net/?retryWrites=true&w=majority";
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(connectionStr, { useNewUrlparser: true })
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log(err));
 
-mongoose.connect(connectionStr, {useNewUrlparser: true})
-.then(() => console.log('connected to mongodb'))
-.catch(err => console.log(err))
-
-mongoose.connection.on('error', err => {
-  console.log(err)
-})
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
