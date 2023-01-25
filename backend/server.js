299baa47dedede992a9867f9b2d2155f378fe08c
PatrykAszurkiewicz <patrykos65a@gmail.com>
2023-01-25 01:57:25 +0100

    f

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   // cors: "http://localhost:3001",
-  cors: "https://itshop-sklep.herokuapp.com",
+  cors: "https://itshop-sklep.herokuapp.com:8080",
   methods: ["GET", "POST", "PATCH", "DELETE"],
 });
 
